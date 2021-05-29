09c2c7adcf1722c16140d4f8cbd64b9a8591465c
Yunus Emre Altanay <y.emrealtanay@gmail.com>
2021-05-29 16:16:32 +0300

    Add files via upload

---
+++ p003_boocommerce_YunusEmreAltanay/product_02.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="tr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+      crossorigin="anonymous"
+    
+    />
+    <link href="main.css" rel="stylesheet">
+
+    <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
+
+    <title>Ürünler</title>
+  </head>
+  <body>
+    
+    <div class="container">
+        <div class="row">
+          <div class="col-12">
+              <!-- Menü Başlangıç -->
+              <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Apple Market</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                      <li class="nav-item">
+                        <a class="nav-link pe-5" href="#">Ürünler</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link pe-5" href="#">Kampanyalar</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link pe-5" href="#">Mağazalar</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link pe-5" href="#">İletişim</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+              <!-- Menü Bitiş -->
+          </div>
